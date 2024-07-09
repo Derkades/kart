8402e2e1b9dfb5456231027ec4a38c99c872a74f
Derkades <robin+github@rkslot.nl>
2024-07-09 13:42:40 +0200

    Add patch scripts and patch file

---
+++ hoverboard-patch-apply.sh
@@ -0,0 +1,4 @@
+# clear local unstaged changes
+cd hoverboard-firmware-hack-FOC
+git checkout .
+git apply ../hoverboard.patch
---
+++ hoverboard-patch-create.sh
@@ -0,0 +1,2 @@
+cd hoverboard-firmware-hack-FOC
+git diff --ignore-space-change > ../hoverboard.patch
